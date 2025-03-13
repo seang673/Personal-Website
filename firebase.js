46356a6a0a77d250dec28daf0a3558d06282cc76
seang673 <seangriffiths673@gmail.com>
2025-03-13 13:34:21 -0400

    Firebase Deployment

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import {initializeApp} from 'firebase/app';
+import {getAnalytics} from "firebase/analytics";
+import {getFirestore} from "firebase/firestore";
+
+//Web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBjXNuA4TJxoyvD9PvJB55mUv9nHJMI7V8",
+    authDomain: "personal-website-de16c.firebaseapp.com",
+    projectId: "personal-website-de16c",
+    storageBucket: "personal-website-de16c.firebasestorage.app",
+    messagingSenderId: "556687835593",
+    appId: "1:556687835593:web:f53a9e86bbf4398baa6818"
+};
+
+//Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export{db};
