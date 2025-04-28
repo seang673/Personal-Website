819ca0dc7948c3d856fc20b29c9b4672b9dbf56d
seang673 <seangriffiths673@gmail.com>
2025-04-27 22:11:41 -0400

    Fixing feedback

--- public/scripts/feedbackFunctions.js
+++ public/scripts/feedbackFunctions.js
@@ -1,4 +1,4 @@
-import { getFirestore, where, collection, addDoc, serverTimestamp, query, orderBy, onSnapshot, getDocs } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { getFirestore, where, collection, addDoc, serverTimestamp, query, orderBy, onSnapshot, getDocs, deleteDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 const auth = getAuth(window.app);
 const db = getFirestore(window.app);
@@ -54,7 +54,14 @@ onAuthStateChanged(auth, (user) => {
                 const time = data.timestamp?.seconds 
                 ? new Date(data.timestamp.seconds * 1000).toLocaleString()
                 : "Pending timestamp";
-                feedbackItem.textContent = `${data.feedback} - ${time}`;
+                feedbackItem.innerHTML = `<pre>                                       <button class="deleteFeedback" style="background-color: red;
+                                          color:white; font-size: 12px; border: none; padding: 8px 12 px; cursor:pointer; transform: translateY(-15px);
+                                           border-radius:5px;">✖</button></pre>
+                                          <strong>${data.feedback}</strong><br>
+                                         <pre style="font-family: 'Courier New', monospace;" >            ${time}</pre>`;
+                feedbackItem.querySelector(".deleteFeedback").addEventListener("click", () => {
+                    deleteDoc(doc.ref);
+                });
                 feedbackContainer.appendChild(feedbackItem);
             });
         });
